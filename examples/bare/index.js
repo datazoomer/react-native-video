78db8e71c37a9871dae3977c7ee68045d52b4e27
dhupararchit <archit.dhupar@viaplaygroup.com>
2025-09-29 18:36:55 +0200

    New screen is added

--- examples/bare/index.js
+++ examples/bare/index.js
@@ -3,9 +3,11 @@
  */
 
 import {AppRegistry} from 'react-native';
+import InitialScreen from 'common/InitialScreen';
 import BasicExample from 'common/BasicExample';
 import {name as appName} from './app.json';
 import DRMExample from 'common/DRMExample';
 
-AppRegistry.registerComponent(appName, () => BasicExample);
+AppRegistry.registerComponent(appName, () => InitialScreen);
+AppRegistry.registerComponent('BasicExample', () => BasicExample);
 AppRegistry.registerComponent('DRMExample', () => DRMExample);
